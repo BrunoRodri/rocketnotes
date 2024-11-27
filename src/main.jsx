28c5861e2b20239aebbe98345fb26daf8b7c030f
BrunoRodri <fidbruno@hotmail.com>
2024-11-27 15:36:16 -0300

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global.js'
+import theme from './styles/theme.js'
+
+import { Profile } from './pages/Profile'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Profile />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
